4a576ae75b5a6b17386b0ec20d018ae3f0d71249
Harshith-Sai-Tumu <harshith.sai2003t@gmail.com>
2024-05-06 19:53:57 +0530

    Add Responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navigationbar from "./components/Navbar/Navigationbar"
+import "./layout.scss"
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+       <Navigationbar/>  
+    </div>
   )
 }
 
---
+++ src/components/Navbar/Navigationbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./navbar.scss";
+
+const Navigationbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setOpen(prevOpen => !prevOpen);
+    };
+
+    return (
+        <nav>
+            <div className="left">
+                <a href="/" className="logo">
+                    <img src="./MainLogo.png" alt="" />
+                    <span>Crestview Cornerstone</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/">Sign in</a>
+                <a className="register" href="/">Sign up</a>
+                <div className="menuIcon">
+                    <img src="/menu.png" alt="" onClick={toggleMenu} />
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign in</a>
+                    <a href="/">Sign up</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navigationbar;
