010831e9211ee160dcf43aa1d0c17c6898540d47
Harshith-Sai-Tumu <harshith.sai2003t@gmail.com>
2024-05-11 19:49:22 +0530

    Add Login and Register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
-import Navigationbar from "./components/Navbar/Navigationbar"
+import Navigationbar from "./components/Navbar/Navigationbar";
 
-import Home from "./routes/Home/Home"
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
-import List from "./routes/list/List"
-import Layout from "./routes/layout/Layout"
-import SinglePage from "./routes/singlePage/SinglePage"
-import Profile from "./routes/profile/Profile"
+import Home from "./routes/Home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import List from "./routes/list/List";
+import Layout from "./routes/layout/Layout";
+import SinglePage from "./routes/singlePage/SinglePage";
+import Profile from "./routes/profile/Profile";
+import Login from "./routes/loginPage/Login";
+import Register from "./routes/register/register";
 
 function App() {
-
-
-   const router = createBrowserRouter([
-   {
-      path:"/",
-      element: <Layout/>,
-      children:[
-         {
-            path:"/",
-            element:<Home/>
-         },
-         {
-            path:"/List",
-            element:<List/>
-         },
-         {
-            path:"/:id",
-            element:<SinglePage/>
-         },
-         {
-            path:"/profile",
-            element:<Profile/>
-
-         }
-      ]
-   }
-   ])
-  return ( 
-
-   <RouterProvider router={router}/>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/List",
+          element: <List />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navigationbar.jsx
+++ src/components/Navbar/Navigationbar.jsx
@@ -3,26 +3,26 @@ import "./navbar.scss";
 import { Link } from "react-router-dom";
 
 const Navigationbar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setOpen(prevOpen => !prevOpen);
-    };
-    const user = true;
-    return (
-        <nav>
-            <div className="left">
-                <a href="/" className="logo">
-                    <img src="./logo.png" alt="" />
-                    <span>Crestview Cornerstone</span>
-                </a>
-                <Link to="/">Home</Link>
-                <a href="/">About</a>
-                <a href="/">Contact</a>
-                <a href="/">Agents</a>
-            </div>
-            <div className="right">
-            {user ? (
+  const toggleMenu = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+  const user = true;
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="./logo.png" alt="" />
+          <span>Crestview Cornerstone</span>
+        </a>
+        <Link to="/">Home</Link>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        {user ? (
           <div className="user">
             <img
               src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
@@ -30,34 +30,32 @@ const Navigationbar = () => {
             />
             <span>John Doe</span>
             <Link to="/profile" className="profile">
-                <div className="notification">
-                    3   
-                </div>
+              <div className="notification">3</div>
               <span>Profile</span>
             </Link>
           </div>
         ) : (
           <>
-            <a href="/">Sign in</a>
-            <a href="/" className="register">
+            <Link to="/login">Sign in</Link>
+            <Link a="/register" className="register">
               Sign up
-            </a>
+            </Link>
           </>
         )}
-                <div className="menuIcon">
-                    <img src="/menu.png" alt="" onClick={toggleMenu} />
-                </div>
-                <div className={open ? "menu active" : "menu"}>
-                    <a href="/">Home</a>
-                    <a href="/">About</a>
-                    <a href="/">Contact</a>
-                    <a href="/">Agents</a>
-                    <a href="/">Sign in</a>
-                    <a href="/">Sign up</a>
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={toggleMenu} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <Link to="/">Home</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+          <Link to="/">Agents</Link>
+          <Link to="/login">Sign in</Link>
+          <Link to="/register">Sign up</Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigationbar;
